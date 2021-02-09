174f8847e5b6e658305fb50b92a47698a025e48d
Israel Teneda <israteneda@gmail.com>
2021-02-09 01:13:19 -0500

    [VER-64] - [DOC] Enhance index page in API documentation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,39 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 const features = [
   {
-    title: 'Fácil de usar',
-    imageUrl: 'img/crear.svg',
+    title: "Fácil de usar",
+    imageUrl: "img/facil-usar.svg",
     description: (
       <>
-        Veronica permite crear cualquier tipo de comprobante electrónico en formato JSON o XML.
+        Veronica permite crear cualquier tipo de comprobante electrónico en
+        formato JSON o XML.
       </>
     ),
   },
   {
-    title: 'Integración completa',
-    imageUrl: 'img/enviar.svg',
+    title: "Integración completa",
+    imageUrl: "img/integracion.svg",
     description: (
       <>
-        Veronica envia y autoriza sus comprobantes electrónicos ante el SRI de manera instantánea y transparente.
+        Veronica envia y autoriza sus comprobantes electrónicos ante el SRI de
+        manera instantánea y transparente.
       </>
     ),
   },
-  // {
-  //   title: 'Personalización',
-  //   imageUrl: 'img/enviar.svg',
-  //   description: (
-  //     <>
-  //       Verónica notificará sus comprobantes a sus clientes de manera personalizada.
-  //     </>
-  //   ),
-  // },
   {
-    title: 'Disponibilidad',
-    imageUrl: 'img/consultar.svg',
+    title: "Disponibilidad",
+    imageUrl: "img/disponibilidad.svg",
     description: (
       <>
-        Verónica almacena de manera segura sus comprobantes para ser consultados en el momento que lo precise.
+        Verónica almacena de manera segura sus comprobantes para ser consultados
+        en el momento que lo precise.
       </>
     ),
   },
@@ -47,15 +41,15 @@ const features = [
 
 const supporters = [
   {
-      imgPath: "/veronica-api-doc/img/clientes/distarias.png",
-      website: "#",
+    imgPath: "/veronica-api-doc/img/clientes/distarias.png",
+    website: "#",
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={clsx('col col--4', styles.feature)}>
+    <div className={clsx("col col--4", styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
@@ -67,37 +61,39 @@ function Feature({imageUrl, title, description}) {
   );
 }
 
-function Customers({}) {
+function Customers() {
   return (
     <div className="container">
-      <div className="row mt-4">
-        <div className="col heading font_large text--black text--center">
-          <h2>Empresas que confian en nosotros</h2>
+      <div className={styles.title}>
+        <h2>Empresas que confian en nosotros</h2>
+      </div>
+      <div className={styles.customers}>
+        {supporters.map((s, i) => (
+          <div key={i} className="align-items-center">
+            <a href={s.website}>
+              <img className={styles.customerImage} src={s.imgPath} />
+            </a>
           </div>
-          </div>
-          <div className="row text-center align-items-center ml-2 mr-2">
-            {supporters.map((s, i) => (
-            <div key={i} className="col-lg-3 col-sm-6 col-6">
-              <a href={s.website}>
-                <img className={styles.customerImage} src={s.imgPath}/>
-              </a>
-            </div>
-                )
-            )}
+        ))}
       </div>
     </div>
   );
 }
 
-function Overview({}) {
+function Overview() {
   return (
-    <div className="container">
-      <div className="row mt-4">
-        <div className="col heading font_large text--black text--center">
-          <h2>¿Por qué somos la API de facturación electrónica más completa?</h2>
-          Verónica API está diseñada para integrarse con cualquier lenguaje de programación y/o flujo de trabajo. Su diseño desacoplado, modular y basado en estándares brinda a los usuarios la capacidad de elegir qué componentes utilizar dentro de sus procesos contables.
-          <img src="https://i.imgur.com/2KlcAcI.png"/>
-        </div>
+    <div className={styles.overview}>
+      <h2 className={styles.title}>
+        ¿Por qué somos la API de facturación electrónica más completa?
+      </h2>
+      <div className="container">
+      <p>
+        Verónica API está diseñada para integrarse con cualquier lenguaje de
+        programación y/o flujo de trabajo. Su diseño desacoplado, modular y
+        basado en estándares brinda a los usuarios la capacidad de elegir qué
+        componentes utilizar dentro de sus procesos contables.
+      </p>
+      <img src="https://i.imgur.com/2KlcAcI.png" />
       </div>
     </div>
   );
@@ -105,22 +101,24 @@ function Overview({}) {
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title={siteConfig.title}
-      description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className={clsx("hero hero--primary", styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <Link
               className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
+                "button button--outline button--secondary button--lg",
+                styles.getStarted
               )}
-              to={useBaseUrl('docs/')}>
+              to={useBaseUrl("docs/")}
+            >
               Comenzar
             </Link>
           </div>
@@ -135,16 +133,12 @@ function Home() {
                   <Feature key={idx} {...props} />
                 ))}
               </div>
-              <div className="row">
-                <Customers></Customers>
-              </div>
-              <div className="row">
-                <Overview></Overview>
-              </div>
             </div>
           </section>
         )}
       </main>
+      <Customers></Customers>
+      <Overview></Overview>
     </Layout>
   );
 }
